d2fd62b63bfb822750bff8cfa35f0e742ca382b1
berkagmpp <berkagmpp@gmail.com>
2022-05-11 13:00:27 +1200

    add age input and entire form validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,8 +4,20 @@ const SimpleInput = (props) => {
     const [enteredName, setEnteredName] = useState('');
     const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
+    const [enteredAge, setEnteredAge] = useState('');
+    const [enteredAgeTouched, setEnteredAgeTouched] = useState(false);
+
     const enteredNameIsValid = enteredName.trim() !== '' && enteredName.trim().length !== 0;
-    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched; 
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+    const enteredAgeIsValid = enteredAge.trim() !== '' && enteredAge.trim().length !== 0 && enteredAge > 0;
+    const ageInputIsInvalid = !enteredAgeIsValid && enteredAgeTouched;
+
+    let formIsValid = false;
+
+    if (enteredNameIsValid && enteredAgeIsValid) {
+        formIsValid = true;
+    };
 
     const nameInputChangeHandler = event => {
         setEnteredName(event.target.value);
@@ -15,36 +27,58 @@ const SimpleInput = (props) => {
         setEnteredNameTouched(true);
     };
 
+    const ageInputChangeHandler = event => {
+        setEnteredAge(event.target.value);
+    };
+
+    const ageInputBlurHandler = event => {
+        setEnteredAgeTouched(true);
+    };
+
     const formSubmissionHandler = event => {
         event.preventDefault();     // we don't want to reload, restart the app and loose state!
 
         setEnteredNameTouched(true);
+        setEnteredAgeTouched(true);
 
-        if (!enteredNameIsValid) {
+        if (!enteredNameIsValid || !enteredAgeIsValid) {
             return;
         }
 
         console.log(enteredName);
+        console.log(enteredAge);
 
         setEnteredName('');     // this, useState() is ideal then useRef()(nameInputRef.current.value = '';) for cleaning becouse useRef() directly manipulate DOM
         setEnteredNameTouched(false);
+        setEnteredAge('');
+        setEnteredAgeTouched(false);
     };
         
     const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
+    const ageInputClasses = ageInputIsInvalid ? 'form-control invalid' : 'form-control';
 
     return (
         <form onSubmit={formSubmissionHandler}>
             <div className={nameInputClasses}>
                 <label htmlFor='name'>Your Name</label>
                 <input type='text' 
-                       id='name' 
-                       onChange={nameInputChangeHandler} 
-                       onBlur={nameInputBlurHandler} 
-                       value={enteredName} />
+                    id='name' 
+                    onChange={nameInputChangeHandler} 
+                    onBlur={nameInputBlurHandler} 
+                    value={enteredName} />
                 {nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
             </div>
+            <div className={ageInputClasses}>
+                <label htmlFor='age'>Your Age</label>
+                <input type='number' 
+                        id='age' 
+                        onChange={ageInputChangeHandler} 
+                        onBlur={ageInputBlurHandler} 
+                        value={enteredAge} />
+                {ageInputIsInvalid && <p className="error-text">Age must not be empty and greater then 0.</p>}
+            </div>
             <div className="form-actions">
-                <button>Submit</button>
+                <button disabled={!formIsValid}>Submit</button>
             </div>
         </form>
     );
